8ef116a50232373563d9085f0126ae753980ed74
jfw123 <569302016@qq.com>
2018-04-18 10:14:04 +0800

    Initial commit

---
+++ app/src/main/java/com/zhongwenhui/tdin360/surface_view/App.java
@@ -0,0 +1,11 @@
+package com.zhongwenhui.tdin360.surface_view;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
